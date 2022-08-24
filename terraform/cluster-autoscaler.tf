efa658d41e555cd8c343ceb8d6f0c1fef3edae97
Towhid Khandaker <towhidk2@live.com>
2022-08-24 17:14:47 +0600

    updates

---
+++ terraform/cluster-autoscaler.tf
@@ -0,0 +1,32 @@
+resource "aws_iam_policy" "autoscaler" {
+    name        = "${var.eks_cluster_name}-autoscaling-policy"
+    description = "EKS AWS autoscaling policy for the cluster ${var.eks_cluster_name}"
+
+# Terraform's "jsonencode" function converts a
+# Terraform expression result to valid JSON syntax.
+    policy = jsonencode({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "autoscaling:DescribeAutoScalingGroups",
+                    "autoscaling:DescribeAutoScalingInstances",
+                    "autoscaling:DescribeLaunchConfigurations",
+                    "autoscaling:DescribeTags",
+                    "autoscaling:SetDesiredCapacity",
+                    "autoscaling:TerminateInstanceInAutoScalingGroup",
+                    "ec2:DescribeLaunchTemplateVersions"
+                ],
+                "Resource": "*"
+            }
+        ]
+    })
+
+}
+
+# custom policy to be attached with existing nodegroup(eks-node-group-role) IAM role which was created in eks module
+resource "aws_iam_role_policy_attachment" "autoscaler" {
+    role       = aws_eks_node_group.node.name
+    policy_arn = aws_iam_policy.autoscaler.arn
+}
